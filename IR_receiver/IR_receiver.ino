1d9d5e87e045acafb5e42a446de2ead3c9838c33
iroj <iroj.gurung86@gmail.com>
2021-08-25 22:09:38 -0400

    arduino recevr with lcd

--- IR_receiver/IR_receiver.ino
+++ IR_receiver/IR_receiver.ino
@@ -1,15 +1,22 @@
 #include <IRremote.h>
+#define STATUS_LED_PIN 7
+#define IR_RECEIVE_PIN 6
 
-const int IR_RECEIVE_PIN = 2;
-bool toggle = false;
+#include <LiquidCrystal.h>
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+
 decode_type_t sony = SONY;
-
 void setup()
 {
   Serial.begin(9600);
+  lcd.begin(16,2);
   IrReceiver.enableIRIn();
   IrReceiver.begin(IR_RECEIVE_PIN, ENABLE_LED_FEEDBACK);
-  pinMode(7, OUTPUT);
+  pinMode(STATUS_LED_PIN, OUTPUT);
+
+  lcd.print("   Iroj's IR ");
+  lcd.setCursor(0,1); //Display position
+  lcd.print("  Code Detector");
 }
 
 void loop()
@@ -18,23 +25,25 @@ void loop()
   if (IrReceiver.decode())
   {
     IrReceiver.printIRResultShort(&Serial);
-    IrReceiver.printIRResultRawFormatted(&Serial);
-    IrReceiver.compensateAndPrintIRResultAsCArray(&Serial);
-    Serial.println(IrReceiver.decodedIRData.decodedRawData);
-
+     lcd.clear();
+    lcd.print("Protocol:             ");
+    lcd.setCursor(10,0);
+    lcd.print(getProtocolString(IrReceiver.decodedIRData.protocol));
+    lcd.setCursor(0,1);
+    lcd.print("Code:                   ");
+    lcd.setCursor(6,1);
+    lcd.print(IrReceiver.decodedIRData.decodedRawData, HEX);
     if (IrReceiver.decodedIRData.protocol == sony)
     {
       if (IrReceiver.decodedIRData.decodedRawData == 2085)
       {
-        digitalWrite(7, HIGH);
+        digitalWrite(STATUS_LED_PIN, LOW);
       }
       if (IrReceiver.decodedIRData.decodedRawData == 2153)
       {
-        digitalWrite(7, LOW);
+        digitalWrite(STATUS_LED_PIN, HIGH);
       }
     }
     IrReceiver.resume();
   }
-
-  delay(1000);
 }
